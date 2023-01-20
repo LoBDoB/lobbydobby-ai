78f85b26154e082b81f651d7c3eb459b2a6ab6d5
ktaehyun <k_taehyun94@naver.com>
2023-01-20 16:37:18 +0900

    update_runFile

--- d_knn.py
+++ d_knn.py
@@ -16,33 +16,44 @@ class lectureKNN(DataPreprocessing):
 
 
     def recommenderLecture(self, target_uid, lecture):
-        recommender = {'prio': [], 'mid': [], 'mino': [], 'else': []}
+        lst = [0 for _ in range(12)]
+        prio, mid, mino, els = lst, lst, lst, lst
         cnt, result = 0, []
         target_lecture = self.matrix[target_uid][:12]
         for lect in lecture:
             for idx, lec in enumerate(lect):
                 if target_lecture[idx]==0 and lec==10:
-                    recommender['prio'].append(idx)
+                    prio[idx] += 1
                 elif target_lecture[idx]==0 and (lec==5 or lec==4 or lec==3):
-                    recommender['mid'].append(idx)
+                    mid[idx] += 1
                 elif lec>=2 and target_lecture[idx]<lec:
-                    recommender['mino'].append(idx)
+                    mino[idx] += 1
                 elif target_lecture[idx] < lec:
-                    recommender['else'].append(idx)
+                    els[idx] += 1
         del target_lecture, lecture
 
-        for key in recommender:
+        for idx in range(12):  # prio, mid, mino, els
             try:
-                recommend = recommender[key]
-                tmp = []
-                for i in set(deepcopy(recommend)):
-                    tmp.append([i, recommend.count(i)])
+                prio_tmp, mid_tmp, mino_tmp, els_tmp = [], [], [], []
+                if prio[idx] != 0:
+                    prio_tmp.append((idx, prio[idx]))
+                elif mid[idx] != 0:
+                    mid_tmp.append((idx, mid[idx]))
+                elif mino[idx] != 0:
+                    mino_tmp.append((idx, mino[idx]))
+                elif els[idx] != 0:
+                    els_tmp.append((idx, els[idx]))
 
-                tmp.sort(key=lambda x: x[1], reverse=True)
+                prio_tmp.sort(key=lambda x: x[1], reverse=True)
+                mid_tmp.sort(key=lambda x: x[1], reverse=True)
+                mino_tmp.sort(key=lambda x: x[1], reverse=True)
+                els_tmp.sort(key=lambda x: x[1], reverse=True)
+                tmp = prio_tmp + mid_tmp + mino_tmp + els_tmp
 
-                for rcm in tmp:
-                    result.append(self.lectures[rcm[0]])
-                    cnt += 1
+                for i, v in tmp:
+                    if i not in result:
+                        result.append(self.lectures[i])
+                        cnt += 1
                     if cnt == 3:
                         return result
             except:
@@ -54,11 +65,11 @@ class lectureKNN(DataPreprocessing):
     def preprocessingSimilarity(self, uid, lst):
         cnt, lecture_lst = 0, []
         for l in lst:
-            if l[1] >= 0.5:
+            if l[1] >= 0.6:
                 cnt += 1
                 user_uid = l[0]
                 lecture_lst.append(self.matrix[user_uid][:12])
-                if cnt == 2:
+                if cnt == 3:
                     return self.recommenderLecture(uid, lecture_lst)
             else:
                 return self.recommenderLecture(uid, lecture_lst)
@@ -73,7 +84,7 @@ class lectureKNN(DataPreprocessing):
             new_temp.append((idx, similarity))
         new_temp.sort(key=lambda x: x[1], reverse=True)
         self.matrix[new_uid] = new_vec
-        print(new_temp[:5])
+        print('\n', new_temp[:3])
         return self.preprocessingSimilarity(new_uid, new_temp)
 
 
@@ -84,5 +95,49 @@ class lectureKNN(DataPreprocessing):
                 similarity = self.cosineSimillarity(vec, self.matrix[user_uid])
                 exist_temp.append((idx, similarity))
         exist_temp.sort(key=lambda x: x[1], reverse=True)
-        print(exist_temp[:5])
+        print(exist_temp[:3])
+        tmp1, tmp2, tmp3 = [], [], []
+        for i, t1, t2, t3 in zip([0,1,2,3,4,5], self.matrix[exist_temp[0][0]][-6:], self.matrix[exist_temp[1][0]][-6:], self.matrix[exist_temp[2][0]][-6:]):
+            if t1 != 0:
+                if i == 0:
+                    tmp1.append('공학')
+                elif i == 1:
+                    tmp1.append('인문/사회')
+                elif i == 2:
+                    tmp1.append('예체능')
+                elif i == 3:
+                    tmp1.append('교육')
+                elif i == 4:
+                    tmp1.append('자연')
+                elif i == 5:
+                    tmp1.append('의약')
+            if t2 != 0:
+                if i == 0:
+                    tmp2.append('공학')
+                elif i == 1:
+                    tmp2.append('인문/사회')
+                elif i == 2:
+                    tmp2.append('예체능')
+                elif i == 3:
+                    tmp2.append('교육')
+                elif i == 4:
+                    tmp2.append('자연')
+                elif i == 5:
+                    tmp2.append('의약')
+            if t3 != 0:
+                if i == 0:
+                    tmp3.append('공학')
+                elif i == 1:
+                    tmp3.append('인문/사회')
+                elif i == 2:
+                    tmp3.append('예체능')
+                elif i == 3:
+                    tmp3.append('교육')
+                elif i == 4:
+                    tmp3.append('자연')
+                elif i == 5:
+                    tmp3.append('의약')
+        print(f'{exist_temp[0][0]}', tmp1)
+        print(f'{exist_temp[1][0]}', tmp2)
+        print(f'{exist_temp[2][0]}', tmp3)
         return self.preprocessingSimilarity(user_uid, exist_temp)
--- run.py
+++ run.py
@@ -1,11 +1,29 @@
 from d_knn import lectureKNN
+from dataPreprocessing import DataPreprocessing
 
+
+temp = DataPreprocessing()
 
 knn = lectureKNN()
 
 for i in range(20):
+    tmp = []
     exist_result = knn.existUser(f'user{i+1}')
-    print(f'user{i+1}')
+    for idx, t in enumerate(temp.matrix[f'user{i+1}'][-6:]):
+        if t != 0:
+            if idx == 0:
+                tmp.append('공학')
+            elif idx == 1:
+                tmp.append('인문/사회')
+            elif idx == 2:
+                tmp.append('예체능')
+            elif idx == 3:
+                tmp.append('교육')
+            elif idx == 4:
+                tmp.append('자연')
+            elif idx == 5:
+                tmp.append('의약')
+    print(f'Target-User => user{i+1}', tmp)
     print(exist_result, '\n')
 
 new = {'user101':[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 15, 0, 5, 0, 0, 0]}
